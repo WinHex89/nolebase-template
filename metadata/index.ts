ddbfd57a446e06625cbb9be86d9b2f0f04078839
自权的SPACE <2895887410@qq.com>
2024-07-03 16:03:10 +0800

    update

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = '自权的SPACE'
+export const siteShortName = '自权的SPACE'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
